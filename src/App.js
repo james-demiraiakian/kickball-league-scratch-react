67409d31a98cebc2ae6d172e35d95a8d85345851
James Demiraiakian <james.armen@gmail.com>
2021-12-16 15:41:17 -0800

    added NavLinks and Routes. they work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kickball League</h1>
+      <BrowserRouter>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/team-list">Teams</NavLink>
+        <NavLink to="/player-list">Players</NavLink>
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/team-list" />
+          <Route path="/team-list/:id" />
+          <Route path="/player-list" />
+          <Route path="/player-list/:id" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
